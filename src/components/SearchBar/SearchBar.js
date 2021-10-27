902a7c7f95966cc15ebbda6ca638f49f7e4719e2
Adam <victor-sopot@users.noreply.github.com>
2021-10-27 11:27:13 +0300

    Completed section 3
    Finished part 3 ravenous - with sample data

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -14,6 +14,10 @@ class SearchBar extends React.Component {
 			'Highest Rated': 'rating',
 			'Most Reviewed': 'review_count'
 		};
+
+		this.handleTermChange = this.handleTermChange.bind(this);
+		this.handleLocationChange = this.handleLocationChange.bind(this);
+		this.handleSearch = this. handleSearch.bind(this);
 	}
 
 	getSortByClass (sortByOption) {
@@ -42,6 +46,11 @@ class SearchBar extends React.Component {
 		});
 	}
 
+	handleSearch (event) {
+		this.props.searchYelp(this.state.term, this.state.location, this.state.sortBy);
+		event.preventDefault();
+	}
+
 	renderSortByOptions() {
 		return Object.keys(this.sortByOptions).map(sortByOption => {
 			let sortByOptionValue = this.sortByOptions[sortByOption];
@@ -58,11 +67,11 @@ class SearchBar extends React.Component {
 			    </ul>
 			  </div>
 			  <div className="SearchBar-fields">
-			    <input placeholder="Search Businesses" />
-			    <input placeholder="Where?" />
+			    <input onChange={this.handleTermChange} placeholder="Search Businesses" />
+			    <input onChange={this.handleLocationChange} placeholder="Where?" />
 			  </div>
 			  <div className="SearchBar-submit">
-			    <a>Let's Go</a>
+			    <a onClick={this.handleSearch}>Let's Go</a>
 			  </div>
 			</div>
 		)
